b3ad3ed84d4bd03374b8ea61cf6fd2bb7f2ab380
Tristan Maxson <tgmaxson@gmail.com>
2026-06-29 22:08:37 -0500

    Restore simple docs sidebar styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_theme_options = {
     "page_width": "875px",
-    "sidebar_width": "280px",
+    "sidebar_width": "260px",
 }
 
 autodoc_typehints = "description"
