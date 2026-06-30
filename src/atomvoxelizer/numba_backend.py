f78bc1f84da15e5d60bd46e59343c18bf2b3f211
Tristan Maxson <tgmaxson@gmail.com>
2026-06-29 19:44:49 -0500

    Optimize Numba backend indexing

--- src/atomvoxelizer/numba_backend.py
+++ src/atomvoxelizer/numba_backend.py
@@ -1,54 +1,49 @@
 from __future__ import annotations
 
 import numpy as np
-from numba import njit, prange
+from numba import njit
 
 from .voxelgrid import VoxelGrid, _cached_sphere_offsets
 
 
 @njit
-def _set_sphere_offsets(grid, center_idx, offsets, value):
-    nx, ny, nz = grid.shape
+def _set_sphere_offsets_flat(grid, center_idx, offsets, value, nx, ny, nz):
     for n in range(offsets.shape[0]):
         x = (center_idx[0] + offsets[n, 0]) % nx
         y = (center_idx[1] + offsets[n, 1]) % ny
         z = (center_idx[2] + offsets[n, 2]) % nz
-        grid[x, y, z] = value
+        grid[(x * ny + y) * nz + z] = value
 
 
 @njit
-def _add_sphere_offsets(grid, center_idx, offsets, value):
-    nx, ny, nz = grid.shape
+def _add_sphere_offsets_flat(grid, center_idx, offsets, value, nx, ny, nz):
     for n in range(offsets.shape[0]):
         x = (center_idx[0] + offsets[n, 0]) % nx
         y = (center_idx[1] + offsets[n, 1]) % ny
         z = (center_idx[2] + offsets[n, 2]) % nz
-        grid[x, y, z] += value
+        grid[(x * ny + y) * nz + z] += value
 
 
 @njit
-def _mul_sphere_offsets(grid, center_idx, offsets, factor):
-    nx, ny, nz = grid.shape
+def _mul_sphere_offsets_flat(grid, center_idx, offsets, factor, nx, ny, nz):
     for n in range(offsets.shape[0]):
         x = (center_idx[0] + offsets[n, 0]) % nx
         y = (center_idx[1] + offsets[n, 1]) % ny
         z = (center_idx[2] + offsets[n, 2]) % nz
-        grid[x, y, z] *= factor
+        grid[(x * ny + y) * nz + z] *= factor
 
 
 @njit
-def _div_sphere_offsets(grid, center_idx, offsets, divisor):
-    nx, ny, nz = grid.shape
+def _div_sphere_offsets_flat(grid, center_idx, offsets, divisor, nx, ny, nz):
     for n in range(offsets.shape[0]):
         x = (center_idx[0] + offsets[n, 0]) % nx
         y = (center_idx[1] + offsets[n, 1]) % ny
         z = (center_idx[2] + offsets[n, 2]) % nz
-        grid[x, y, z] /= divisor
+        grid[(x * ny + y) * nz + z] /= divisor
 
 
 @njit
-def _set_many_sphere_offsets(grid, center_indices, offsets, value):
-    nx, ny, nz = grid.shape
+def _set_many_sphere_offsets_flat(grid, center_indices, offsets, value, nx, ny, nz):
     for c in range(center_indices.shape[0]):
         cx = center_indices[c, 0]
         cy = center_indices[c, 1]
@@ -57,12 +52,11 @@ def _set_many_sphere_offsets(grid, center_indices, offsets, value):
             x = (cx + offsets[n, 0]) % nx
             y = (cy + offsets[n, 1]) % ny
             z = (cz + offsets[n, 2]) % nz
-            grid[x, y, z] = value
+            grid[(x * ny + y) * nz + z] = value
 
 
 @njit
-def _add_many_sphere_offsets(grid, center_indices, offsets, value):
-    nx, ny, nz = grid.shape
+def _add_many_sphere_offsets_flat(grid, center_indices, offsets, value, nx, ny, nz):
     for c in range(center_indices.shape[0]):
         cx = center_indices[c, 0]
         cy = center_indices[c, 1]
@@ -71,20 +65,7 @@ def _add_many_sphere_offsets(grid, center_indices, offsets, value):
             x = (cx + offsets[n, 0]) % nx
             y = (cy + offsets[n, 1]) % ny
             z = (cz + offsets[n, 2]) % nz
-            grid[x, y, z] += value
-
-
-@njit(parallel=True)
-def _clamp_grid(grid, min_val, max_val):
-    nx, ny, nz = grid.shape
-    for i in prange(nx):
-        for j in range(ny):
-            for k in range(nz):
-                v = grid[i, j, k]
-                if v < min_val:
-                    grid[i, j, k] = min_val
-                elif v > max_val:
-                    grid[i, j, k] = max_val
+            grid[(x * ny + y) * nz + z] += value
 
 
 class VoxelGridNumba(VoxelGrid):
@@ -92,44 +73,54 @@ class VoxelGridNumba(VoxelGrid):
 
     backend_name = "numba"
 
+    def _flat_grid_and_shape(self):
+        nx, ny, nz = self.grid.shape
+        return self.grid.ravel(), int(nx), int(ny), int(nz)
+
     def set_sphere(self, center, radius, value=1):
         center_idx = self._center_index(center)
         offsets = self._sphere_offsets(radius)
-        _set_sphere_offsets(self.grid, center_idx, offsets, value)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
+        _set_sphere_offsets_flat(grid, center_idx, offsets, value, nx, ny, nz)
 
     def add_sphere(self, center, radius, value=1):
         center_idx = self._center_index(center)
         offsets = self._sphere_offsets(radius)
-        _add_sphere_offsets(self.grid, center_idx, offsets, value)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
+        _add_sphere_offsets_flat(grid, center_idx, offsets, value, nx, ny, nz)
 
     def mul_sphere(self, center, radius, factor=2):
         center_idx = self._center_index(center)
         offsets = self._sphere_offsets(radius)
-        _mul_sphere_offsets(self.grid, center_idx, offsets, factor)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
+        _mul_sphere_offsets_flat(grid, center_idx, offsets, factor, nx, ny, nz)
 
     def div_sphere(self, center, radius, factor=2):
         center_idx = self._center_index(center)
         offsets = self._sphere_offsets(radius)
-        _div_sphere_offsets(self.grid, center_idx, offsets, factor)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
+        _div_sphere_offsets_flat(grid, center_idx, offsets, factor, nx, ny, nz)
 
     def add_spheres(self, centers, radii, value=1):
         centers, radii = self._validate_spheres(centers, radii)
         center_indices = self.positions_to_indices(centers)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
         for radius in np.unique(radii):
             offsets = _cached_sphere_offsets(float(radius), tuple(self.gpts), tuple(map(tuple, self.cell)))
             subset = center_indices[radii == radius]
-            _add_many_sphere_offsets(self.grid, subset, offsets, value)
+            _add_many_sphere_offsets_flat(grid, subset, offsets, value, nx, ny, nz)
 
     def set_spheres(self, centers, radii, value=1):
         centers, radii = self._validate_spheres(centers, radii)
         center_indices = self.positions_to_indices(centers)
+        grid, nx, ny, nz = self._flat_grid_and_shape()
         for radius in np.unique(radii):
             offsets = _cached_sphere_offsets(float(radius), tuple(self.gpts), tuple(map(tuple, self.cell)))
             subset = center_indices[radii == radius]
-            _set_many_sphere_offsets(self.grid, subset, offsets, value)
+            _set_many_sphere_offsets_flat(grid, subset, offsets, value, nx, ny, nz)
 
     def clamp_grid(self, min_val=0.0, max_val=1.0):
-        _clamp_grid(self.grid, min_val, max_val)
+        np.clip(self.grid, min_val, max_val, out=self.grid)
 
 
 __all__ = ["VoxelGridNumba"]
