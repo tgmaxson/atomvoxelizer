972ce5ecb174383301ade0080e3d22fd987abb46
Tristan Maxson <tgmaxson@gmail.com>
2026-06-29 22:03:32 -0500

    Fix Read the Docs layout styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ html_logo = "_static/logo.png"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_theme_options = {
-    "page_width": "100%",
+    "page_width": "875px",
     "sidebar_width": "280px",
 }
 
